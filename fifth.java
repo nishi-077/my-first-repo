19658a6a2a0841427af330b0f79acc8631647998
nishi-077 <nishit.mca25@siescoms.sies.edu.in>
2026-03-04 20:16:41 +0530

    Create fifth.java

---
+++ fifth.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class hello{
+    public static void main(String args[]){
+        System.out.println("Welcome Nishi");
+        System.out.println("This is original content");
+    }
+}
