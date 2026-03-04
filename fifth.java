b90902dd9fd8bf9c04b98ee71cff473ed00453f9
nishi-077 <nishit.mca25@siescoms.sies.edu.in>
2026-03-04 20:37:41 +0530

    Create fifth.java

--- fifth.java
+++ fifth.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class fifth{
     public static void main(String args[]){
         System.out.println("Welcome Nishi");
-        System.out.println("This is original content");
+        System.out.println("This is original content from git");
     }
 }
