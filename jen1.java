76ecb3c6e412ccdb093bc489f77bbf2e183fe78b
nishi-077 <nishit.mca25@siescoms.sies.edu.in>
2026-03-04 14:09:33 +0530

    Update jen1.java

--- jen1.java
+++ jen1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class jen1{
   public static void main(String args[]){
   System.out.println("Hello This is Jenkins project");
-  System.out.println("Hello Student");
+  System.out.println("Hello Nishi");
   
   }
 }
