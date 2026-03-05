e9bc1afcbd32a573fe057430da4b12f70e46e59e
nishi-077 <nishit.mca25@siescoms.sies.edu.in>
2026-03-05 14:34:27 +0530

    Update fifth.java

--- fifth.java
+++ fifth.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class fifth{
     public static void main(String args[]){
-        System.out.println("Welcome Student");
+        System.out.println("Welcome Nishi");
         System.out.println("The content is now updated on git");
     }
 }
