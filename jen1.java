0e0446641e8198b7e201a68d05446101708d63ef
nishi-077 <nishit.mca25@siescoms.sies.edu.in>
2026-03-07 13:17:54 +0530

    Update jen1.java

--- jen1.java
+++ jen1.java
@@ -4,6 +4,6 @@ public class jen1{
   public static void main(String args[]){
   System.out.println("Hello This is Jenkins project");
   System.out.println("Hello Nishi");
-  
+  System.out.println("THIS IS UPDATED FOR WEBHOOKS");
   }
 }
