e25c644c9c9a645811db70cf094b6f9405047eba
nishi-077 <nishit.mca25@siescoms.sies.edu.in>
2026-03-07 15:54:51 +0530

    Update fifth.java

--- fifth.java
+++ fifth.java
@@ -3,5 +3,6 @@ public class fifth{
     public static void main(String args[]){
         System.out.println("Hello Nishi ,Welcome");
         System.out.println("THIS IS JAVA FILE FOR DEVOPS QUESTION 8");
+        System.out.println("File Updated");
     }
 }
